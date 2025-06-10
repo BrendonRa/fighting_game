9b74d0403ee4204988b32b77df40c1acedf97741
Brendon Rabelo dos Santos <105553904+BrendonRa@users.noreply.github.com>
2025-06-10 07:24:05 -0300

    samurai add

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ const spriteE = "img/characters/samurai/idle/idle1.png"
 const scene = new Image()
 scene.src = "img/scenes/forest/background_forest.png"
 
-let temp
 const gravity = 0.2
+let anim
 
 // Creation
 class Sprite {
-    constructor ({position, color, size, velocity, state}) {
+    constructor ({position, sprite, size, velocity, state, direction, frameAnim}) {
         this.position = position
-        this.color = color
-        this.image = new Image()
-        this.image.src = this.color
+        this.sprite = sprite
         this.size = size
         this.velocity = velocity
         this.state = state
+        this.direction = direction
+        this.frameAnim = frameAnim
     }
 
     draw() {
-        if (this.state == "idle") {
-            c.drawImage(this.image, this.position.x, this.position.y, this.size.dx, this.size.dy)
-        }
+        this.image = new Image()
+        this.image.src = this.sprite[Math.ceil(this.frameAnim / 20)]
+        c.drawImage(this.image, this.position.x, this.position.y, this.size.dx, this.size.dy)
     }
 
     update() {
@@ -44,16 +44,65 @@ class Sprite {
         if (this.position.y + this.velocity.y + this.size.dy - 10 >= canvas.height) {     
             this.velocity.y = 0
         } else this.velocity.y += gravity
+
+        this.velocity.x = 0
+
+        if (this.position.x >= edge.left) {
+            this.velocity.x = 3 * this.direction.x
+        }
+        
+        if (this.position.x <= edge.right) {
+            this.velocity.x = 3 * this.direction.x
+        }
+
+        /*if (keys.jumpP.pressed && this.velocity.y <= 0) {
+            this.velocity.y = -6 * this.direction.y
+        }*/
+
+        if (this.velocity.x != 0 && this.state != "jump" && this.state == "idle") this.sprite = samurai.walk
+        if (this.velocity.x == 0 && this.state != "jump") this.sprite = samurai.idle
+
+        this.frameAnim++
+        if (this.frameAnim >= (this.sprite.length - 1) * 20) this.frameAnim = 0
+
+        console.log(this.frameAnim)
     }
 }
+
+
 
 // Characters
 
 const samurai = {
-    idle: [],
-    walk: [],
-    attack: [],
-    death: []
+    idle: ["img/characters/samurai/idle/idle1.png", 
+        "img/characters/samurai/idle/idle2.png", 
+        "img/characters/samurai/idle/idle3.png", 
+        "img/characters/samurai/idle/idle4.png", 
+        "img/characters/samurai/idle/idle5.png", 
+        "img/characters/samurai/idle/idle6.png"
+    ],
+    walk: ["img/characters/samurai/walk/walk1.png", 
+        "img/characters/samurai/walk/walk2.png", 
+        "img/characters/samurai/walk/walk3.png", 
+        "img/characters/samurai/walk/walk4.png", 
+        "img/characters/samurai/walk/walk5.png", 
+        "img/characters/samurai/walk/walk6.png", 
+        "img/characters/samurai/walk/walk7.png", 
+        "img/characters/samurai/walk/walk8.png", 
+        "img/characters/samurai/walk/walk9.png"
+    ],
+    attack: ["img/characters/samurai/attack/attack1.png",
+        "img/characters/samurai/attack/attack2.png",
+        "img/characters/samurai/attack/attack3.png",
+        "img/characters/samurai/attack/attack4.png"
+    ],
+    death: ["img/characters/samurai/dead/dead1.png",
+        "img/characters/samurai/dead/dead2.png",
+        "img/characters/samurai/dead/dead3.png",
+        "img/characters/samurai/dead/dead4.png",
+        "img/characters/samurai/dead/dead5.png",
+        "img/characters/samurai/dead/dead6.png"
+    ]
 }
 
 const player = new Sprite({
@@ -61,7 +110,7 @@ const player = new Sprite({
         x: canvas.width / 6,
         y: 100
     },
-    color: spriteP,
+    sprite: samurai.idle,
     size: {
         dx: 192,
         dy: 192
@@ -70,7 +119,12 @@ const player = new Sprite({
         x: 0,
         y: 0
     },
-    state: "idle"
+    state: "idle",
+    direction: {
+        x: 0,
+        y: 0
+    },
+    frameAnim: 0
 })
 
 const player2 = new Sprite({
@@ -78,7 +132,7 @@ const player2 = new Sprite({
         x: (canvas.width / 3) * 2,
         y: 100
     },
-    color: spriteE,
+    sprite: samurai.idle,
     size: {
         dx: 192,
         dy: 192
@@ -87,7 +141,12 @@ const player2 = new Sprite({
         x: 0,
         y: 0
     },
-    state: "idle"
+    state: "idle",
+    direction: {
+        x: 0,
+        y: 0
+    },
+    frameAnim: 0
 })
 
 // sides canva
@@ -97,40 +156,19 @@ const edge = {
 }
 
 // imputs
-const keys = {
-    leftP: {
-        pressed: false
-    },
-    rightP: {
-        pressed: false
-    },
-    jumpP: {
-        pressed: false
-    },
-    leftE: {
-        pressed: false
-    },
-    rightE: {
-        pressed: false
-    },
-    jumpE: {
-        pressed: false
-    },
-}
-
 window.addEventListener("keydown", function (event) {
     switch (event.key) {
         case "d":
-            keys.rightP.pressed = true
+            player.direction.x = 1
             break
         case "a":
-            keys.leftP.pressed = true
+            player.direction.x = -1
             break
         case "ArrowLeft":
-            keys.leftE.pressed = true
+            player2.direction.x = -1
             break
         case "ArrowRight":
-            keys.rightE.pressed = true
+            player2.direction.x = 1
             break
     }
 
@@ -145,16 +183,16 @@ window.addEventListener("keydown", function (event) {
 window.addEventListener("keyup", function (event) {
     switch (event.key) {
         case "d":
-            keys.rightP.pressed = false
+            player.direction.x = 0
             break
         case "a":
-            keys.leftP.pressed = false
+            player.direction.x = 0
             break
         case "ArrowLeft":
-            keys.leftE.pressed = false
+            player2.direction.x = 0
             break
         case "ArrowRight":
-            keys.rightE.pressed = false
+            player2.direction.x = 0
             break
     }
 })
@@ -167,40 +205,5 @@ function animate() {
     
     player.update()
     player2.update()
-
-    player.velocity.x = 0
-    player2.velocity.x = 0
-
-    if (keys.leftP.pressed) {
-        if (player.position.x >= edge.left) {
-            player.velocity.x = -3
-        }
-    }
-    
-    if (keys.rightP.pressed) {
-        if (player.position.x <= edge.right) {
-            player.velocity.x = 3
-        }
-    }
-
-    if (keys.leftE.pressed) {
-        if (player2.position.x >= edge.left) {
-            player2.velocity.x = -3
-        }
-    }
-
-    if (keys.rightE.pressed) {
-        if (player2.position.x <= edge.right) {
-            player2.velocity.x = 3
-        }
-    }
-    
-    if (keys.jumpP.pressed && player.velocity.y <= 0) {
-        player.velocity.y = -6
-    }
-
-    if (keys.jumpE.pressed && player2.velocity.y <= 0) {
-        player2.velocity.y = -6
-    }
 }
 animate()
